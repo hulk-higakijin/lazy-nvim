a8d70ed45cc79454e7fe5d165565270674d5328b
higakijin <higakijin@higakijins-MacBook-Air.local>
2024-07-31 22:33:26 +0900

    feat: accept copilot's suggestion instead of tab

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -6,8 +6,20 @@ return {
       { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim" },
     },
-    opts = {
-      debug = true,
-    },
+    config = function()
+      vim.g.copilot_no_tab_map = true
+
+      local keymap = vim.keymap.set
+      keymap(
+        "i",
+        "<C-e>",
+        'copilot#Accept("<CR>")',
+        { silent = true, expr = true, script = true, replace_keycodes = false }
+      )
+      keymap("i", "<C-j>", "<Plug>(copilot-next)")
+      keymap("i", "<C-k>", "<Plug>(copilot-previous)")
+      keymap("i", "<C-o>", "<Plug>(copilot-dismiss)")
+      keymap("i", "<C-s>", "<Plug>(copilot-suggest)")
+    end,
   },
 }
