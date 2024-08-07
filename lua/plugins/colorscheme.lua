7827c6fddfa9079a26b419f325ee6f50bfcea41d
higakijin <higakijin@higakijins-MacBook-Air.local>
2024-08-07 20:34:22 +0900

    feat: colorscheme flow

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
 return {
   { "rebelot/kanagawa.nvim" },
   {
+    "0xstepit/flow.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "midnight",
-      colorscheme = "kanagawa",
+      colorscheme = "flow"
     },
   },
 }
+
