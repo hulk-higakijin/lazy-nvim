626a5cc325aee360b1e80f1e0a43b64a9fcf1333
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-12 22:04:35 +0900

    feat: remove whitespace at end of line when file have saved

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,12 @@
 vim.g.autoformat = false
 vim.opt.relativenumber = false
 vim.opt.number = true
+
+-- remove whitespace when file have saved
+vim.api.nvim_create_augroup("RemoveTrailingWhitespace", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = "RemoveTrailingWhitespace",
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
